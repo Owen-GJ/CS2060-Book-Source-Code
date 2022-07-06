c076a30076818d2d26f4d46f20c6243409d91026
Owen-GJ <owen_gjohnson@yahoo.com>
2022-07-06 10:39:47 -0600

    Update fig07_28.c
    Changed functions to return int values.

--- examples/ch07/fig07_28.c
+++ examples/ch07/fig07_28.c
@@ -3,47 +3,52 @@
 #include <stdio.h>
 
 // prototypes
-void function1(int a);
-void function2(int b);
-void function3(int c);
+int function1(int a);
+int function2(int b);
+int function3(int c);
 
 int main(void)
 {
    // initialize array of 3 pointers to functions that each take an
    // int argument and return void                              
-   void (*f[3])(int) = {function1, function2, function3};    
+   int (*f[3])(int) = {function1, function2, function3};    
 
    printf("%s", "Enter a number between 0 and 2, 3 to end: ");
    size_t choice; // variable to hold user's choice
-   scanf("%u", &choice);
+   scanf("%llu", &choice);
+
+   //variable to catch the return of function
+   int catch = 0;
 
    // process user's choice
    while (choice >= 0 && choice < 3) {
 
       // invoke function at location choice in array f and pass
       // choice as an argument                              
-      (*f[choice])(choice);                                
+      catch = (*f[choice])(choice);                                
 
       printf("%s", "Enter a number between 0 and 2, 3 to end: ");
-      scanf("%u", &choice);
+      scanf("%llu", &choice);
    } 
+
+   printf("The return value is %d", catch);
 
    puts("Program execution completed.");
 } 
 
-void function1(int a)
+int function1(int a)
 {
-   printf("You entered %d so function1 was called\n\n", a);
+    return a;
 }
 
-void function2(int b)
+int function2(int b)
 {
-   printf("You entered %d so function2 was called\n\n", b);
+   return b;
 }
 
-void function3(int c)
+int function3(int c)
 {
-   printf("You entered %d so function3 was called\n\n", c);  
+    return c;
 }
 
 
