3cea9806f4f1430208ed4aef050b497fbc015522
Owen-GJ <owen_gjohnson@yahoo.com>
2022-07-24 13:02:25 -0600

    Started it

---
+++ HW/GarrettJohnsonOwenHW05.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+#define NAME_LENGTH 80
+
+
+struct pet {
+	char name[NAME_LENGTH];
+	int age;
+};
+
+typedef struct pet Pet;
+
+int main(void) {
+
+	struct 
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
